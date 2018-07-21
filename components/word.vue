169fb47b8f2aecf22936c2950189665ff08271df
Mergen Ganbat <mergenfromrabbit@gmail.com>
2018-07-22 01:20:15 +0800

    what a lovely day

--- components/word.vue
+++ components/word.vue
@@ -2,31 +2,7 @@
   <div></div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        activeNames: [''],
-        con: false,
-        ttype: ''
-      }
-    },
-    methods: {
-      handleChange(val) {
-        if(val.length > 1 && this.ttype == ''){
-          this.ttype = '1'
-          this.con = true
-        }
-      },
-      conti(val){
-        this.ttype = String(val)
-        this.activeNames = ['1']
-        this.con = true
-      },
-      nextWord(val){
-        console.log(val)
-      }
-    }
-  }
+  
 </script>
 <style lang="stylus" scoped>
   .el-collapse
--- pages/secret/_id.vue
+++ pages/secret/_id.vue
@@ -17,16 +17,17 @@
       </el-dropdown>
     </el-header>
     <el-main>
-      <el-col :span="12">
+      Your mastered words: {{mastered}}
+      <el-col v-if="workingOn.word != 'loading'" :span="12">
         <el-card shadow="hover">
           <div class="list-item">
-            {{workingOn[it].word}}
+            {{workingOn.word}}
             <br>
-            <div v-if="!(theWord.syllables === undefined)">
+            <div v-if="!(workingOn.syllables === undefined)">
               <p class="para">
                 Syllables: 
-                {{theWord.syllables.count}} |
-                <span v-for="syl in theWord.syllables.list" :key="syl">
+                {{workingOn.syllables.count}} |
+                <span v-for="syl in workingOn.syllables.list" :key="syl">
                   -{{syl}}
                 </span>
               </p>
@@ -34,7 +35,7 @@
           </div>
           <el-collapse v-model="activeNames" @change="handleChange">
             <el-collapse-item title="Click here if you give up and want to see hint!" name="1" @click="conti(1)">
-              <div class="list-item" v-for="(item, index) in theWord.results" :key="index">
+              <div class="list-item" v-for="(item, index) in workingOn.results" :key="index">
                 <span v-if="!(item.definition === undefined)">Definition {{index + 1}} {{item.partOfSpeech}} : {{item.definition}}</span>
                 <br>
                 <span v-if="!(item.synonyms === undefined)">Synonyms {{index + 1}} : <span v-if="item.synonyms" v-for="synonym in item.synonyms" :key="synonym"> {{synonym}} </span></span>
@@ -90,17 +91,12 @@ export default {
       url: '',
       token: '',
       it: 0,
-      workingOn: [{word: "loading"}],
+      workingOn: {word: "loading"},
+      wordId: null,
       activeNames: [''],
       con: false,
       ttype: '',
-      theWord: {
-        syllables: {
-          count: 0,
-          list: []
-        },
-        results: []
-      },
+      mastered: 0,
       data: {
 
       }
@@ -114,53 +110,37 @@ export default {
       console.log('socket')
     },
     news: function(val){
-      var tmp = val.data.workingOn
-      console.log(tmp)
-      var i;
-      for(i = 0; i < tmp.length; i++){
-        if(tmp[i].scored == true) continue;
-        else {
-          this.it = i;
-          break;
-        }
-      }
-      if(i == tmp.length){
-        console.log('deep shit')
-        this.socket.emit("finished", {day: this.url, token: this.token})
-      }
-      this.workingOn = val.data.workingOn
-      this.data = {
-        round: val.data.round,
-        start: val.data.start,
-        end: val.data.end,
-        newwords: val.data.newwords,
-        oldwords: val.data.oldwords
-      }
-      this.$socket.emit('word', {token: this.token, day: this.url, word: this.workingOn[this.it].word})
+      console.log(val)
+      this.$socket.emit('word', {day: this.url, token: this.token, wordId: val.wordId})
+      this.wordId = val.wordId
     },
     word: function(val){
       console.log(val)
-      this.theWord = {
-        syllables: val.data.syllables,
-        results: val.data.results
-      }
+      this.workingOn = val.theWord
+      this.$socket.emit('mastered', {day: this.url, token: this.token})
     },
-    progress: function(val){
+    mastered: function(val){
+      this.mastered = val.mastered;
+    },
+    error: function(val){
       console.log(val)
-      if(val.saved == true){
-        console.log("hellow")
-        this.it = this.it+1
-        if(this.it == this.workingOn.length){
-          this.workingOn.push({word: "Loading!"})
-          this.$socket.emit('finished', {day: this.url, token: this.token})
-        } else {
-          this.$socket.emit('word', {token: this.token, day: this.url, word: this.workingOn[this.it].word})
-        }
-        this.activeNames = ['']
-        this.con = false
-        this.ttype = ''
-      }
     }
+    // progress: function(val){
+    //   console.log(val)
+    //   if(val.saved == true){
+    //     console.log("hellow")
+    //     this.it = this.it+1
+    //     if(this.it == this.workingOn.length){
+    //       this.workingOn.push({word: "Loading!"})
+    //       this.$socket.emit('finished', {day: this.url, token: this.token})
+    //     } else {
+    //       this.$socket.emit('word', {token: this.token, day: this.url, word: this.workingOn[this.it].word})
+    //     }
+    //     this.activeNames = ['']
+    //     this.con = false
+    //     this.ttype = ''
+    //   }
+    // }
   },
   mounted(){
     this.url = this.$route.params.id
@@ -181,39 +161,11 @@ export default {
       this.con = true
     },
     nextWord(val){
-      console.log(val)
-      console.log(this.ttype)
-      var tmp = Number(this.ttype)
-      if(tmp > 2){
-        tmp = 1
-      } else {
-        tmp = 0
-      }
-      if(val == 2) tmp = 0
-      this.workingOn[this.it].scored = true
-      var ress = Math.floor(this.it/4)
-      if(tmp == 0) ress = -1
-      this.workingOn[this.it].ans = String(ress + 1)
-      var j, tmp;
-      for(j = 0; j < this.data.newwords.length; j++){
-        if(this.data.newwords[j].word == this.workingOn[this.it].word){
-          this.data.newwords[j].ans.push({ans: ress + 1})
-          this.data.newwords[j].score += ress+1
-          tmp = this.data.newwords[j]
-          break;
-        }
-      }
-      if(j == this.data.newwords){
-        for(j = 0; j < this.data.oldwords.length; j++){
-          if(this.data.oldwords[j].word == this.workingOn[this.it].word){
-            this.data.oldwords[j].ans.push({ans: ress + 1})
-            this.data.oldwords[j].score += ress+1
-            tmp = this.data.oldwords[j]
-            break;
-          }
-        }
-      }
-      this.$socket.emit('progress', {day: this.url, token: this.token, word: tmp.word, change: tmp, data: {round: this.data.round, start: this.data.start, end: this.data.end, newwords: this.data.newwords, oldwords: this.data.oldwords, workingOn: this.workingOn}})
+      console.log(Number(this.ttype))
+      this.$socket.emit('progress', {day: this.url, token: this.token, result: Number(this.ttype), wordId: this.wordId})
+      this.activeNames = ['']
+      this.con = false
+      this.ttype = ''
     }
   }
 }
